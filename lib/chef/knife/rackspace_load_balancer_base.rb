9fa9578a4793bd15acd8c0e225094e48c390a690
Matt Vermaak <pair+matt@howaboutwe.com>
2012-04-11 09:53:35 -0400

    add region param

--- lib/chef/knife/rackspace_load_balancer_base.rb
+++ lib/chef/knife/rackspace_load_balancer_base.rb
@@ -1,11 +1,20 @@
 class Chef
   class Knife
     module RackspaceLoadBalancerBase
+      def self.included(base)
+        base.class_eval do
+          option :rackspace_api_region,
+            :short => "-R REGION",
+            :long => "--rackspace-api-region REGION",
+            :description => "Your rackspace API region. IE: ord, dfw",
+            :proc => Proc.new {|region| Chef::Config[:knife][:rackspace_api_region] = region}
+        end
+      end
       def rackspace_api_credentials
         {
           :username => Chef::Config[:knife][:rackspace_api_username],
           :api_key => Chef::Config[:knife][:rackspace_api_key],
-          :region => Chef::Config[:knife][:rackspace_region]
+          :region => Chef::Config[:knife][:rackspace_api_region]
         }
       end
 
