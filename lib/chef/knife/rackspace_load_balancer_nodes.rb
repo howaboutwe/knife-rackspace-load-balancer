a8fe6e675e94fe799874c4b3a144103fcf5a2f88
Bryan Woods & Kenneth Lay <pair+bryan+kenneth@howaboutwe.com>
2012-04-23 11:09:40 -0400

    Updates internal node_ip location

--- lib/chef/knife/rackspace_load_balancer_nodes.rb
+++ lib/chef/knife/rackspace_load_balancer_nodes.rb
@@ -12,20 +12,34 @@ class Chef
 
       def resolve_node_ips_from_config(options)
         node_ips = []
+
         if options[:by_search]
           nodes_from_chef = nodes_by_search(options[:by_search])
+          node_ips = find_internal_ip_from_node(nodes_from_chef)
 
-          node_ips = nodes_from_chef.map {|n| n.rackspace.private_ip}
         elsif options[:by_name]
           node_names = options[:by_name].split(",")
-          nodes_from_chef = nodes_by_search(node_names.map {|n| "name:#{n}"}.join(" OR "))
+          nodes_from_chef = nodes_by_search(
+            node_names.map {|n| "name:#{n}"}.join(" OR ")
+          )
 
-          node_ips = nodes_from_chef.map {|n| n.rackspace.private_ip}
+          node_ips = find_internal_ip_from_node(nodes_from_chef)
+
         elsif options[:by_private_ip]
           node_ips = config[:by_private_ip].split(",")
         end
 
         node_ips
+      end
+
+      private
+
+      def find_internal_ip_from_node(nodes)
+        nodes.map do |node|
+          node.network["interfaces"]["eth1"]["addresses"].keys.detect do |ip|
+            ip =~ /10\./
+          end
+        end
       end
     end
   end
